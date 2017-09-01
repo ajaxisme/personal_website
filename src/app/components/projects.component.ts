4728881e4b82b30a1f6d0fe7fe302a6d6f1ca84c
Abhinav Jha <jhaab@us.ibm.com>
2017-09-01 17:27:26 -0400

    added projects

--- src/app/components/projects.component.ts
+++ src/app/components/projects.component.ts
@@ -5,4 +5,36 @@ import { Component } from '@angular/core';
   templateUrl: './projects.component.html'
 })
 export class ProjectComponent {
+  projects: Project[];
+
+  constructor() {
+    this.projects = this.getProjects();
+  }
+
+  private getProjects() {
+    let project_list: Project[] = [
+      {
+        "title" : "URLShortener",
+        "description" : "A url shortener clone developed in NodeJS",
+        "technology" : [ "NodeJS", "JavaScript"],
+        "github_link" : "https://github.com/ajaxisme/URLShortener"
+      },
+      {
+        "title" : "tubix",
+        "description" : "A web video player with lyrics to go along with.",
+        "technology" : [ "Flask", "Python", "React", "JavaScript"],
+        "github_link" : "https://github.com/ajaxisme/tubix"
+      }
+    ];
+
+    return project_list;
+  }
+
+}
+
+interface Project {
+  title: string,
+  description: string;
+  technology: string[];
+  github_link: string;
 }
